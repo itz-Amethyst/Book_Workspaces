81d4d44e1b2d4276d0574ad88c6f12e672160824
itz-amethyst <pransermi@gmail.com>
2024-07-26 21:46:42 +0330

    feat: simple db interaction

--- learning_go/playground/server.go
+++ learning_go/playground/server.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"net/http"
 	"fmt"
 	// "io"
 	"log/slog"
 	"os"
+
+	"github.com/lib/pg"
+)
+
+const (
+	host = "localhost"
+	port = 5432
+	user = "someone"
+	dbname = "test"
+	password = "1234"
 )
 
 type Product struct {
@@ -23,15 +34,48 @@ var generalProduct *Product = &Product{
 
 var jsonHandler *slog.JSONHandler = slog.NewJSONHandler(os.Stderr, nil)
 var myslog *slog.Logger = slog.New(jsonHandler)
+var globalDatabase *sql.DB
+
+func createTables(){
+	rows, err := globalDatabase.Query(`create table if not exists Products(
+		id INT PRIMARY KEY,
+		title TEXT,
+		price INT
+	)`)
+	if err != nil {
+		myslog.Error(err.Error()))
+	}
+	fmt.Println(rows)
+}
 
 func getRoute(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("Got / requests \n")
+	var (
+		id int
+		title string
+		price int
+	)
+	// fmt.Printf("Got / requests \n")
 	product := Product {
 		1,
 		"Chain",
 		2000,
 	}
-	response, _ := json.Marshal(product)
+
+	rows, err := globalDatabase.Query(`select * from Products`)
+	if err != nil {
+		myslog.Error(err.Error())
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		err := rows.Scan(&id, &title, &price)
+		if err != nil {
+			myslog.Error(err.Error())
+		}
+		fmt.Prinln(id, title, price)
+	}
+	fmt.Println(rows)
+	response, _ := json.Marshal(Product{Id: id, Title: title, Price: price})
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(response)
 	products := []Product{*generalProduct}
@@ -41,6 +85,7 @@ func getRoute(w http.ResponseWriter, r *http.Request) {
 }
 
 func createProduct(w http.ResponseWriter, r *http.Request) {
+
 	body := json.NewDecoder(r.Body)
 	p := &Product{}
 	err := body.Decode(p)
@@ -48,13 +93,31 @@ func createProduct(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), 500)
 	}
 	
-	_ = p
+	rows, err := globalDatabase.Query(`insert into Products (id, title, price) values ($1, $2, $3)`, p.Id, p.Title, p.Price)
+	if err != nil {
+		myslog.Error(err.Error())
+	}
+	fmt.Println(rows)
 }
 
 func main(){
 	myslog.Info("Start Listening to 8000")
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s" + "password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 	http.HandleFunc("/", getRoute)
 	http.HandleFunc("/create_product", createProduct)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.close()
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	globalDatabase = db
+	myslog.Info("Start creating tables process")
+	createTables()
 	err := http.ListenAndServe(":8000", nil)
 	if err != nil {
 		myslog.Error("Something went wrong")
