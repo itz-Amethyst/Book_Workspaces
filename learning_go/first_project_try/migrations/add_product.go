8afacc860b5196efebc82aa0fb9707564167d949
itz-amethyst <pransermi@gmail.com>
2024-08-02 23:41:01 +0330

    feat: migration initializer

--- learning_go/first_project_try/migrations/add_product.go
+++ learning_go/first_project_try/migrations/add_product.go
@@ -3,24 +3,41 @@ package migrations
 import (
 	"fmt"
 	"digikala/logger"
-	"digikala/mydb"
+	_ "digikala/mydb"
 	_ "github.com/lib/pq"
+	"digikala/configs"
+	"digikala/models"
 )
+func Setup(){
 
-
-func CreateTables(){
-
-	globalDatabase := mydb.ConnectDatabase()
 	myslog := logger.GetLogger()
-
-	myslog.Info("create tables")
-	rows, err := globalDatabase.Query(`create table if not exists Product (
-  id INT PRIMARY KEY,
-  title TEXT,
-  price INT
-)`)
+	config2, err := configs.LoadConfig(".")
 	if err != nil {
-		myslog.Error(err.Error())
+		myslog.Error("Could not find the env")
 	}
-	fmt.Println(rows)
+
+	configs.ConnectDb(&config2)
 }
+
+func RunMigration(){
+	Setup()
+	configs.DB.AutoMigrate(&models.Product{}, & models.Brand{})
+	fmt.Println("Successfully migrated")
+}
+
+// func CreateTables(){
+
+// 	globalDatabase := mydb.ConnectDatabase()
+// 	myslog := logger.GetLogger()
+
+// 	myslog.Info("create tables")
+// 	rows, err := globalDatabase.Query(`create table if not exists Product (
+//   id INT PRIMARY KEY,
+//   title TEXT,
+//   price INT
+// )`)
+// 	if err != nil {
+// 		myslog.Error(err.Error())
+// 	}
+// 	fmt.Println(rows)
+// }
